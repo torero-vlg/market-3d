5ffacbd64d927d2b72bc1a1425959d8b886c24fe
artem.v.rusanov@gmail.com <artem.v.rusanov@gmail.com@5f035127-2f84-9123-802b-8c5b83e7caf8>
2014-10-02 15:37:37 +0000

    тесты

---
+++ trunk/Db.Test/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using NUnit.Framework;
+
+namespace Db.Test
+{
+    public abstract class BaseTest
+    {
+        protected AbstractDbFactory DbFactory;
+        private static string ConnectionString { get { return ConfigurationManager.ConnectionStrings["DatabaseFile"].ConnectionString; } }
+
+        protected BaseTest()
+        {
+            DbFactory = new NhDbFactory(ConnectionString);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+    }
+}
--- trunk/T034.Db/Mapping/SubjectMap.cs
+++ trunk/T034.Db/Mapping/SubjectMap.cs
@@ -9,6 +9,15 @@ namespace Db.Mapping
         {
             Id(x => x.Id).Column("SubjectId").GeneratedBy.Assigned();
 
+            Map(p => p.Article);
+            Map(p => p.Description);
+            Map(p => p.Count);
+            Map(p => p.Name);
+            Map(p => p.Price);
+
+            References(p => p.Category).Column("CategoryId")
+                .Not.LazyLoad(); 
+
             HasOne<Product>(x => x.Id);
         }
     }
