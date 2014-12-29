5691b6d5e7f2a9c17070fbc4ed2973ebfb89605e
artem.v.rusanov@gmail.com <artem.v.rusanov@gmail.com@5f035127-2f84-9123-802b-8c5b83e7caf8>
2014-12-29 15:29:01 +0000

    новая версия Мигратора. Теперь ему можно подсовывать  dll с маппингом и он по ней будет делать БД + можно передавать параметры через консоль

--- trunk/Migrator/Program.cs
+++ trunk/Migrator/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using Db.Mapping;
+
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using NHibernate.Context;
@@ -17,9 +15,14 @@ namespace Migrator
         {
             try
             {
-                UpdateDb();
-                Console.WriteLine("Операция выполнена успешно.");
+                Parse(args);
 
+                if(Task == Task.Clear)
+                    ClearDb();
+                if (Task == Task.Update)
+                    UpdateDb();
+                
+                Console.WriteLine("Операция выполнена успешно.");
             }
             catch (Exception ex)
             {
@@ -36,16 +39,79 @@ namespace Migrator
                                .Database(SQLiteConfiguration.Standard.ConnectionString(str))
                                .ExposeConfiguration(c => c.Properties.Add("current_session_context_class",
                                                                           typeof(CallSessionContext).FullName))
-                               .Mappings(x => x.FluentMappings.AddFromAssemblyOf<NewsMap>())
+                               //.Mappings(x => x.FluentMappings.AddFromAssemblyOf<NewsMap>())
+                               .Mappings(x => x.FluentMappings.AddFromAssembly(Assembly))
                                .BuildConfiguration();
-
-            //очистим БД
-            //var t = new SchemaExport(cfg2);
-            //t.Execute(true, true, true);
 
             ////обновим структуру
             var t = new SchemaUpdate(cfg2);
             t.Execute(true, true);
         }
+
+        public static void ClearDb()
+        {
+            var str = string.Format("Data Source=t034.sqlite;Version=3;");
+            var cfg2 = Fluently.Configure()
+                               .Database(SQLiteConfiguration.Standard.ConnectionString(str))
+                               .ExposeConfiguration(c => c.Properties.Add("current_session_context_class",
+                                                                          typeof(CallSessionContext).FullName))
+                                //.Mappings(x => x.FluentMappings.AddFromAssemblyOf<NewsMap>())
+                               .Mappings(x => x.FluentMappings.AddFromAssembly(Assembly))
+                               .BuildConfiguration();
+
+            //очистим БД
+            var t = new SchemaExport(cfg2);
+            t.Execute(true, true, true);
+        }
+
+
+#region Перенести в другой класс
+        
+        public static void Parse(string[] args)
+        {
+            for (var i = 0; i < args.Count(); i++)
+            {
+                if (args.Count() > i + 1)
+                    switch (args[i])
+                    {
+                        case "-task":
+                            SetTask(args[i + 1]);
+                            break;
+                        case "-dll":
+                            SetAssembly(args[i + 1]);
+                            break;
+                    }
+            }
+        }
+
+        protected static Task Task { get; set; }
+        protected static Assembly Assembly { get; set; }
+
+        private static void SetTask(string packType)
+        {
+            switch (packType)
+            {
+                case "clear":
+                    Task = Task.Clear;
+                    break;
+                case "update":
+                    Task = Task.Update;
+                    break;
+            }
+        }
+
+        private static void SetAssembly(string path)
+        {
+            Assembly = Assembly.LoadFrom(path);
+        }
+#endregion
+
     }
+
+    public enum Task
+    {
+        Clear = 0,
+        Update = 1
+    };
+
 }
